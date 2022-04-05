68dcbc35cb7454d975928faf6b0601fce1a43a70
roman <mxhx23@gmail.com>
2022-04-05 20:04:25 +0300

    build(dialog): fill dialog and fill message

--- src/main/resources/db/changelog/migrations/009-fill-message-and-dialog.sql
+++ src/main/resources/db/changelog/migrations/009-fill-message-and-dialog.sql
@@ -1,5 +1,4 @@
-INSERT INTO dialog () values
-    (), (), (), (), ();
+DELETE FROM message;
 INSERT INTO message (time, author_id, recipient_id, message_text, read_status, dialog_id) values
     ('2022-03-18 19:10:25-07', 2, 1, 'test', 'SENT', 1),
     ('2022-03-18 19:11:25-07', 2, 1, 'test1', 'SENT', 1),
@@ -9,3 +8,5 @@ INSERT INTO message (time, author_id, recipient_id, message_text, read_status, d
     ('2022-03-18 19:10:09-07', 5, 1, 'test5', 'READ', 4),
     ('2022-03-18 19:09:25-07', 6, 1, 'test6', 'SENT', 5),
     ('2022-03-18 19:10:25-07', 6, 1, 'test7', 'SENT', 5);
+INSERT INTO dialog (id) values
+    (1), (2), (3), (4), (5);
