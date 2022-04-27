a19888b507f19546865267b96618a12bcf274be8
Kirill <euchekavelo@gmail.com>
2022-04-27 17:38:56 +0300

    Added previously removed DefaultController for redirect

--- src/main/java/ru/skillbox/socnetwork/exception/CustomAuthenticationEntryPoint.java
+++ src/main/java/ru/skillbox/socnetwork/exception/CustomAuthenticationEntryPoint.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.util.MimeTypeUtils;
-import ru.skillbox.socnetwork.logging.InfoLogs;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
