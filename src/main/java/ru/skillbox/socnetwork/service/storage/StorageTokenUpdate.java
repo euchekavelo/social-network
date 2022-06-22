2903a4ed1041d1df7fa844285f4de4d5ae70cda8
stolyarenko.d <aieya.mail@gmail.com>
2022-06-22 10:16:43 +0800

    (storage): nwe token update scope

--- src/main/java/ru/skillbox/socnetwork/service/storage/StorageTokenUpdate.java
+++ src/main/java/ru/skillbox/socnetwork/service/storage/StorageTokenUpdate.java
@@ -2,33 +2,42 @@ package ru.skillbox.socnetwork.service.storage;
 
 import com.dropbox.core.DbxException;
 import com.dropbox.core.DbxRequestConfig;
-import com.dropbox.core.json.JsonReader;
 import com.dropbox.core.oauth.DbxCredential;
 import com.dropbox.core.oauth.DbxRefreshResult;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import java.io.File;
 
 @Component
 public class StorageTokenUpdate {
 
   private static DbxCredential credential;
-  private static final String APP_ID = "272m6nu2hx6xsuw";
-  private static final String PATH = "/tmp/credentials.json";
 
-//  public StorageTokenUpdate() throws JsonReader.FileLoadException {
-//    credential = DbxCredential.Reader.readFromFile(new File(PATH));
-//  }
+  @Getter
+  @Value("${skillbox.app.mail.db.appId}")
+  private String appId;
+
+  @Getter
+  @Value("${skillbox.app.mail.db.appSecret}")
+  private String appSecret;
+
+  @Getter
+  @Value("${skillbox.app.mail.db.refreshToken}")
+  private String refreshToken;
+
+  public StorageTokenUpdate() {
+    credential = new DbxCredential("", 0L, refreshToken, appId);
+  }
 
   @Scheduled(fixedRateString = "PT03H", initialDelayString = "PT3M")
   public void refreshToken() throws DbxException {
 
-    DbxRequestConfig config = new DbxRequestConfig(APP_ID);
+    DbxRequestConfig config = new DbxRequestConfig(appId);
     DbxRefreshResult result = credential.refresh(config);
 
-    credential = new DbxCredential(result.getAccessToken(), result.getExpiresAt(), credential.getRefreshToken(),
-            credential.getAppKey(), credential.getAppSecret());
+    credential = new DbxCredential(result.getAccessToken(), result.getExpiresAt(), refreshToken,
+            appId, appSecret);
 
     StorageService.updateToken(result.getAccessToken());
   }
